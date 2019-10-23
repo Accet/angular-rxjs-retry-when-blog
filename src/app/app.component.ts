8b71d7a697c5fcf1caa970693cc16b76cd041f00
Eugene Havrylov <eugeneh@dev6.com>
2019-10-23 14:33:13 -0400

    emulating server response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { from, Observable, of, throwError } from "rxjs";
+import { catchError, concatMap, delay, take, tap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit{
-  output: string;
-  constructor() {
+export class AppComponent implements OnInit {
+  output = [];
+  respMap = [0, 0, 0, 1]; // 0 - error thrown; 1 - successful result
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  fakeAPICall(params: number): Observable<any> {
+    return from(this.respMap).pipe(
+      delay(500),
+      concatMap(res => {
+        this.respMap.shift();
+        if (!res) {
+          return throwError(res);
+        } else {
+          return of(res);
+        }
+      })
+    );
   }
 
-  ngOnInit(): void {
-
+  handleClick() {
+    this.fakeAPICall(1)
+      .pipe(
+        take(1),
+        tap(result => this.output.push(result)),
+        catchError(err => {
+          this.output.push(err);
+          return throwError(err);
+        })
+      )
+      .subscribe();
   }
-
 }
